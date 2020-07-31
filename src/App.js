71c09fd49b135d38d103fd94d984ad69c30b5596
Chandan Shetti <chandanshetti149@gmail.com>
2020-07-31 20:14:07 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React,{useEffect,useState} from 'react';
 import Recipe from './Recipe'
 import './App.css';
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+console.log(process.env.REACT_APP_ID)
+
 const App = ()=> {
-  const APP_ID = "d70e7a05"
-  const APP_KEY = "dd016303f01dc970e4be85822916e009"
-  // const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  // const APP_ID = "d70e7a05"
+  // const APP_KEY = "dd016303f01dc970e4be85822916e009"
+  // const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`;
 
   const [recipes,setRecipes] = useState([])
   const [search,setSearch] = useState('')
@@ -16,10 +19,11 @@ const App = ()=> {
   },[query])
 
   const getRecipes = async()=>{
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`)
     const data = await response.json()
     setRecipes(data.hits);
     console.log(data.hits);
+
   }
 
   const updateSearch = e =>{
@@ -36,7 +40,7 @@ const App = ()=> {
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <input placeholder="Enter a keyword to find your favorite food recipe" className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">Submit</button>
       </form>
       <div className="recipes">
